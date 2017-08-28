0583eb51e806c3a69102397d2374502fa8b97ca2
Manuel Alzugaray <manuel.alzugaray@datamyne.com>
2017-08-28 11:37:04 -0300

    cambio ip postgres

--- src/main/java/common/utils/DataConnect.java
+++ src/main/java/common/utils/DataConnect.java
@@ -9,7 +9,7 @@ public class DataConnect {
 		try {
 			Class.forName("org.postgresql.Driver");
 			Connection con = DriverManager.getConnection(
-					"jdbc:postgresql://localhost:5432/postgresqltsi2db", "user6V6", "udwqxFEjlIEIoJ7k");
+					"jdbc:postgresql://172.31.31.161:5432/postgresqltsi2db", "user6V6", "udwqxFEjlIEIoJ7k");
 			return con;
 		} catch (Exception ex) {
 			System.out.println("Database.getConnection() Error -->"

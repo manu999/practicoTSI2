068a77f569db527c20404cb29215678ac9d81860
Manuel Alzugaray <manuel.alzugaray@datamyne.com>
2017-08-28 14:56:25 -0300

    cambios para usar jpa

--- src/main/java/common/dao/LoginDAO.java
+++ src/main/java/common/dao/LoginDAO.java
@@ -4,33 +4,54 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
+import javax.persistence.Query;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import common.entity.User;
 import common.utils.DataConnect;
 
+
 public class LoginDAO {
-
+	private static final String PERSISTENCE_UNIT_NAME = "Users";
+    private static EntityManagerFactory factory;
+    
+	@SuppressWarnings("unchecked")
 	public static boolean validate(String user, String password) {
-		Connection con = null;
-		PreparedStatement ps = null;
 
+		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        EntityManager em = factory.createEntityManager();
+
+        boolean ret = true;
 		try {
-			con = DataConnect.getConnection();
-			ps = con.prepareStatement("Select uname, password from Users where uname = ? and password = ?");
-			ps.setString(1, user);
-			ps.setString(2, password);
-
-			ResultSet rs = ps.executeQuery();
-
-			if (rs.next()) {
-				//result found, means valid inputs
-				return true;
+			// Select del usuario
+	        Query q = em.createQuery("SELECT u FROM User u");
+	        List<User> userList = q.getResultList();
+	        
+			if (userList.isEmpty()) {
+				// Create new user
+				em.getTransaction().begin();
+				User u = new User();
+				u.setLogin(user);
+				u.setPassword(password);
+				em.persist(user);
+				em.getTransaction().commit();
+				
+			}else{
+				User u = userList.get(0);
+				if(!u.getPassword().equals(password)){
+					ret = false;
+				}
 			}
-		} catch (SQLException ex) {
+		} catch (Exception ex) {
 			System.out.println("Login error -->" + ex.getMessage());
 			return false;
 		} finally {
-			DataConnect.close(con);
+			em.close();
 		}
-		return false;
+		return ret;
 	}
 }
